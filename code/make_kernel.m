77a0d3d212ca6a904eb47b400479041ec62ea3af
mvidell <mvidell@kth.se>
2017-01-09 11:58:27 +0100

    Moved the creation of K to different function and expanded on kpca

--- code/make_kernel.m
+++ code/make_kernel.m
@@ -2,7 +2,7 @@
 % kernelType: kernel type
 % params: kernel parameters (array to allow multiparam kernels)
 % ret: Kernel as an anonymous function
-function kernel=MakeKernel(kernelType, param)
+function kernel=make_kernel(kernelType, param)
   switch kernelType
     case 'rbf' % Radial Basis Function / Gaussian Kernel
       kernel = @(x,y) exp(-1 / param * norm(x-y)^2);
