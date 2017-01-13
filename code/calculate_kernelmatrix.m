33422f6d6f5dfb7f23f2db7441112f3deb3d7450
mvidell <mvidell@kth.se>
2017-01-13 16:05:24 +0100

    Fixed issue with pdist

--- code/calculate_kernelmatrix.m
+++ code/calculate_kernelmatrix.m
@@ -3,10 +3,13 @@
 function K = calculate_kernelmatrix(X, kernel)
   % N: number of samples
   N = size(X, 1);
+
   K = pdist(X, kernel);
   K = squareform(K);
-  self = kernel(X,X); % calculate the kernel with itself
-  K = K + diag(self);
+  for n=1:N
+      K(n, n) = kernel(X(n), X(n));
+  end
+
   % Centralize kernel matrix
   l = ones(N) / N;
   K = K - l*K - K*l + l*K*l;
--- code/make_kernel.m
+++ code/make_kernel.m
@@ -5,7 +5,7 @@
 function kernel=make_kernel(kernelType, param)
   switch kernelType
     case 'rbf' % Radial Basis Function / Gaussian Kernel
-      kernel = @(x,y) exp(-1 / param * sum((x-y).^2, 2));
+      kernel = @(x,y) exp(-1 / param * sum((repmat(x, [size(y, 1), 1]) - y).^2, 2));
     case 'poly' % Polynomial Kernel
       kernel = @(x,y) (sum(x.*y, 2)+1)^param;
   end
