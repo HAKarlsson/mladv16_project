a3c0b0a7bea3724b29ae3d4f95990d33bc5f2f87
Henrik Karlsson <henakka@gmail.com>
2017-01-13 17:46:59 +0100

    Set up usps_test.m

--- code/make_kernel.m
+++ code/make_kernel.m
@@ -3,9 +3,21 @@
 % params: kernel parameters (array to allow multiparam kernels)
 % ret: Kernel as an anonymous function
 function kernel=make_kernel(kernelType, param)
-  switch kernelType
-    case 'rbf' % Radial Basis Function / Gaussian Kernel
-      kernel = @(x,y) exp(-1 / param * sum((repmat(x, [size(y, 1), 1]) - y).^2, 2));
-    case 'poly' % Polynomial Kernel
-      kernel = @(x,y) (sum(x.*y, 2)+1)^param;
+  isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
+  if isOctave
+    % program is octave
+    switch kernelType
+      case 'rbf' % Radial Basis Function / Gaussian Kernel
+        kernel = @(x,y) exp(-1 / param * sum((x - y).^2, 2));
+      case 'poly' % Polynomial Kernel
+        kernel = @(x,y) (sum(x.*y, 2)+1)^param;
+    end
+  else
+    % Program is matlab
+    switch kernelType
+      case 'rbf' % Radial Basis Function / Gaussian Kernel
+        kernel = @(x,y) exp(-1 / param * sum((repmat(x, [size(y, 1), 1]) - y).^2, 2));
+      case 'poly' % Polynomial Kernel
+        kernel = @(x,y) (sum(x.*y, 2)+1)^param; % TODO: adapt to matlab
+    end
   end
--- code/usps_test.m
+++ code/usps_test.m
@@ -6,23 +6,6 @@ function usps_test()
 % trainData: 3000 samples, 300 of each number
 % testData:  500 samples, 50 of each number
 load 'data/usps_data.mat'
-X = testData(:, 2:257); % first column consists of labels
-% specifying kernel to be used
-comp = 64;
-kernelType = 'rbf';
-param = 0.5*comp;
-kernel = make_kernel(kernelType, param);
-
-str = sprintf('data/usps_alpha(%s,%.2f,%d).mat', kernelType, param, comp);
-if ~exist(str) % if we have computed the alpha vectors already
-  % !!! This can take some time.
-  display('Calculating alphas from training data');
-  [~, alpha, ~] = kpca(X, kernel, comp);
-  save(str,'alpha', '-v7')
-else
-  display('Loading alphas from file');
-  load(str)
-end
 
 % usps_noisy_test.mat contains:
 % speckleTest:  test data with "speckle" noise
@@ -30,18 +13,51 @@ end
 % 500 samples, 50 of each number
 load('data/usps_noisy_test.mat')
 
-
-% ==== DENOISING PART ===
-% A noisy sample
-z=[];
-% Now we will denoise x, z is the denoised x
-for i=1:50:451
-  x = gaussianTest(i,2:257);
-  zi = denoise(x, X, alpha, kernel);
-  z = [z;zi];
+X = trainData(:, 2:257); % first column consists of labels
+% specifying kernel to be used
+kernelType = 'rbf';
+param = 0.5 * 256;
+kernel = make_kernel(kernelType, param);
+[N, D] = size(X);
+str = sprintf('data/usps_alpha(%s,%.2f,%d).mat', kernelType, param, D);
+if ~exist(str) % if we have computed the alpha vectors already
+  % !!! This can take some time.
+  display('Calculating alphas from training data');
+  [~, alpha, ~] = kpca(X, kernel, D);
+  save(str, 'alpha', '-v7')
+else
+  display('Loading alphas from file');
+  load(str)
 end
-Im = usps_matrix2images(z);
+
+c = 2;
+z = [];
+for i=1:50:451
+  x = testData(i+c,2:257);
+  z = [z;x];
+end
+Ims = usps_matrix2images(z);
+z = [];
+for i=1:50:451
+  x = gaussianTest(i+c,2:257);
+  z = [z;x];
+end
+Ims = [Ims;usps_matrix2images(z)];
+
+for comp=[1, 4, 16, 64, 256]
+  % ==== DENOISING PART ===
+  % A noisy sample
+  z=[];
+  % Now we will denoise x, z is the denoised x
+  for i=1:50:451
+    x = gaussianTest(i+c,2:257);
+    zi = denoise(x, X, alpha(:,1:comp), kernel);
+    z = [z;zi];
+  end
+  Im = usps_matrix2images(z);
+  Ims = [Ims;Im];
+end
 
 
-I = mat2gray(Im, [1, -1]);
+I = mat2gray(Ims, [1, -1]);
 imshow(I);
