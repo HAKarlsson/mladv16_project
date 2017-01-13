1f5b3ce7a0a5073e64f518d3d0e8d4f40b074110
mvidell <mvidell@kth.se>
2017-01-13 17:57:57 +0100

    Fixed make_kernel

--- code/make_kernel.m
+++ code/make_kernel.m
@@ -3,9 +3,9 @@
 % params: kernel parameters (array to allow multiparam kernels)
 % ret: Kernel as an anonymous function
 function kernel=make_kernel(kernelType, param)
-<<<<<<< HEAD
   isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
-  if isOctave
+  isOctave
+  if isOctave == 1
     % program is octave
     switch kernelType
       case 'rbf' % Radial Basis Function / Gaussian Kernel
@@ -19,6 +19,6 @@ function kernel=make_kernel(kernelType, param)
       case 'rbf' % Radial Basis Function / Gaussian Kernel
         kernel = @(x,y) exp(-1 / param * sum((repmat(x, [size(y, 1), 1]) - y).^2, 2));
       case 'poly' % Polynomial Kernel
-        kernel = @(x,y) (sum(x.*y, 2)+1)^param; % TODO: adapt to matlab
+        kernel = @(x,y) (sum(repmat(x, [size(y, 1), 1]) .* y, 2) + 1).^param;
     end
   end
